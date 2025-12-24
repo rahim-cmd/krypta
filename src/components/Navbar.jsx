e222b5f2780f54901edee1b9941064ce6718f3c2
rahim-cmd <rahim.mohammed1@gmail.com>
2025-12-24 19:36:16 +0530

    navbar fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -56,48 +56,22 @@ function Navbar() {
                       </li>
 
                       <li className={`nav-item nav-link ${isActive("/about")}`}>
-<<<<<<< HEAD
-                        <Link to="/about" onClick={closeMenu}>
-=======
-                        <Link
-                          to="/about"
-                          onClick={closeMenu}
-                          
-                        >
->>>>>>> 837dc6680516993f74a836c49866aa4dff527348
+                      <Link to="/about" onClick={closeMenu}>
                           About Us
                         </Link>
                       </li>
 
-<<<<<<< HEAD
-                      <li
-                        className={`nav-item nav-link ${isActive("/contact")}`}
-                      >
-                        <Link to="/contact" onClick={closeMenu}>
-=======
+
                       <li className={`nav-item nav-link ${isActive("/contact")}`}>
-                        <Link
-                          to="/contact"
-                          onClick={closeMenu}
-                          
-                        >
->>>>>>> 837dc6680516993f74a836c49866aa4dff527348
+                        <Link to="/contact" onClick={closeMenu}>
                           Contact Us
                         </Link>
                       </li>
 
-<<<<<<< HEAD
-                      <li
-                        className={`nav-item nav-link ${isActive("/services")}`}
-                      >
+                      <li className={`nav-item nav-link ${isActive("/services")}`}>
                         <Link to="/services" onClick={closeMenu}>
                           Services
                         </Link>
-=======
-                      <li className={`nav-item nav-link ${isActive("/services")}`}>
-                        <Link to="/services" onClick={closeMenu}>Services</Link>
-                        
->>>>>>> 837dc6680516993f74a836c49866aa4dff527348
                       </li>
 
                       <li>
