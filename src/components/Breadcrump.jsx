c55314188a9dd8036a1bc61739eee27e90c85453
rahim-cmd <rahim.mohammed1@gmail.com>
2025-12-30 12:54:36 +0530

    service updated

--- src/components/Breadcrump.jsx
+++ src/components/Breadcrump.jsx
@@ -1,52 +1,85 @@
-import React from 'react';
+import React from "react";
 import { useLocation, Link } from "react-router-dom";
 
 export default function Breadcrumb() {
-    const location = useLocation();
-    const path = location.pathname; // e.g., "/about"
+  const location = useLocation();
+  const path = location.pathname; // e.g., "/about"
 
-    if (path === '/about') {
-        return (
-            <div className="container-fluid" style={{background:'orange'}}>
-                <div className="container text-center py-5" style={{ maxWidth: "900px" }}>
-                    <h4 className="text-white  mb-4 wow fadeInDown" data-wow-delay="0.1s">About</h4>
-                    <ol className="breadcrumb d-flex justify-content-center mb-0 wow fadeInDown" data-wow-delay="0.3s">
-                        <li className="breadcrumb-item"><Link to="/">Home</Link></li>
-                        <li className="breadcrumb-item"><Link to="#">pages</Link></li>
-                        <li className="active text-secondary">{path}</li>
-                    </ol>
-                </div>
-            </div>
-        );
-    }else if(path === '/contact'){
+  if (path === "/about") {
+    return (
+      <div className="container-fluid" style={{ background: "orange" }}>
+        <div
+          className="container text-center py-5"
+          style={{ maxWidth: "900px" }}
+        >
+          <h4 className="text-white  mb-4 wow fadeInDown" data-wow-delay="0.1s">
+            About
+          </h4>
+          <ol
+            className="breadcrumb d-flex justify-content-center mb-0 wow fadeInDown"
+            data-wow-delay="0.3s"
+          >
+            <li className="breadcrumb-item">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="breadcrumb-item">
+              <Link to="#">pages</Link>
+            </li>
+            <li className="active text-secondary">{path}</li>
+          </ol>
+        </div>
+      </div>
+    );
+  } else if (path === "/contact") {
+    return (
+      <div className="container-fluid" style={{ background: "orange" }}>
+        <div
+          className="container text-center py-5"
+          style={{ maxWidth: "900px" }}
+        >
+          <h4 className="text-white  mb-4 wow fadeInDown" data-wow-delay="0.1s">
+            Contact
+          </h4>
+          <ol
+            className="breadcrumb d-flex justify-content-center mb-0 wow fadeInDown"
+            data-wow-delay="0.3s"
+          >
+            <li className="breadcrumb-item">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="breadcrumb-item">
+              <Link to="#">pages</Link>
+            </li>
 
-           return (<div className="container-fluid" style={{background:'orange'}}>
-                <div className="container text-center py-5" style={{ maxWidth: "900px" }}>
-                    <h4 className="text-white  mb-4 wow fadeInDown" data-wow-delay="0.1s">Contact</h4>
-                    <ol className="breadcrumb d-flex justify-content-center mb-0 wow fadeInDown" data-wow-delay="0.3s">
-                        <li className="breadcrumb-item"><Link to="/">Home</Link></li>
-                        <li className="breadcrumb-item"><Link to="#">pages</Link></li>
-                        
-                        <li className="active text-secondary">{path}</li>
-                    </ol>
-                </div>
-            </div>)
-
-    }else if(path === '/'){
-        return null;
-    } else{
-
-return (
-            <div className="container-fluid" style={{background:'orange'}}>
-                <div className="container text-center py-5" style={{ maxWidth: "900px" }}>
-                    <h4 className="text-white mb-4 wow fadeInDown" data-wow-delay="0.1s">Services</h4>
-                    <ol className="breadcrumb d-flex justify-content-center mb-0 wow fadeInDown" data-wow-delay="0.3s">
-                        <li className="breadcrumb-item"><Link to="/">Home</Link></li>
-                        <li className="breadcrumb-item">pages</li>
-                        <li className="active text-secondary">{path}</li>
-                    </ol>
-                </div>
-            </div>
-);
-    }
+            <li className="active text-secondary">{path}</li>
+          </ol>
+        </div>
+      </div>
+    );
+  } else if (path === "/") {
+    return null;
+  } else {
+    return (
+      <div className="container-fluid" style={{ background: "orange" }}>
+        <div
+          className="container text-center py-5"
+          style={{ maxWidth: "900px" }}
+        >
+          <h4 className="text-white mb-4 wow fadeInDown" data-wow-delay="0.1s">
+            Services
+          </h4>
+          <ol
+            className="breadcrumb d-flex justify-content-center mb-0 wow fadeInDown"
+            data-wow-delay="0.3s"
+          >
+            <li className="breadcrumb-item">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="breadcrumb-item">pages</li>
+            <li className="active text-secondary">{path}</li>
+          </ol>
+        </div>
+      </div>
+    );
+  }
 }
